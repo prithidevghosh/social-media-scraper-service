71659cc578860acfefff60304887d4e73c00c2e1
Prithidev Ghosh <prithidevghosh@gmail.com>
2024-04-21 23:30:57 +0530

    feat: added some remaining features

--- app/services/twitter/service_twitter_user_follower_list.py
+++ app/services/twitter/service_twitter_user_follower_list.py
@@ -5,8 +5,7 @@ from datetime import datetime
 
 
 from ...utils.logger import logger
-# from fastapi.logger import logger
-from ...utils.twitter_db_operation import retrieve_user_creds
+from ...utils.fetch_user_id import fetch_user_id
 
 from ...services.twitter import service_twitter_login
 
@@ -59,44 +58,6 @@ def url_dispenser(user_id, verification_filter, last_iteration_output_count = No
             elif last_iteration_output_count is None:
                 return f"https://twitter.com/i/api/graphql/SAuX47ZqVkz9Y-4gEsdp0A/Followers?variables=%7B%22userId%22%3A%22{user_id}%22%2C%22count%22%3A20%2C%22includePromotedContent%22%3Afalse%7D&features=%7B%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22rweb_video_timestamps_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
 
-
-
-
-
-def fetch_user_id(session, x_csrf_token, username):
-    logger.info(f"***{SERVICE_NAME}::: fetching user id for the username: {username}")
-
-    url = f"https://twitter.com/i/api/graphql/k5XapwcSikNsEsILW5FvgA/UserByScreenName?variables=%7B%22screen_name%22%3A%22{username}%22%2C%22withSafetyModeUserFields%22%3Atrue%7D&features=%7B%22hidden_profile_likes_enabled%22%3Atrue%2C%22hidden_profile_subscriptions_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Atrue%2C%22subscriptions_verification_info_is_identity_verified_enabled%22%3Atrue%2C%22subscriptions_verification_info_verified_since_enabled%22%3Atrue%2C%22highlights_tweets_tab_ui_enabled%22%3Atrue%2C%22responsive_web_twitter_article_notes_tab_enabled%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%7D&fieldToggles=%7B%22withAuxiliaryUserLabels%22%3Afalse%7D"
-
-    payload = {}
-    headers = {
-    'Host': 'twitter.com',
-    'Sec-Ch-Ua': '"Chromium";v="119", "Not?A_Brand";v="24"',
-    'X-Twitter-Client-Language': 'en',
-    'X-Csrf-Token': f'{x_csrf_token}',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Authorization': f'{BEARER_TOKEN}',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.199 Safari/537.36',
-    'Content-Type': 'application/json',
-    'X-Twitter-Auth-Type': 'OAuth2Session',
-    'X-Twitter-Active-User': 'yes',
-    'Sec-Ch-Ua-Platform': '"Linux"',
-    'Accept': '/',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': f'https://twitter.com/{username}',
-    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'Priority': 'u=1, i',
-    }
-
-    response = session.get(url, headers=headers, data=payload)
-
-    json_response = response.json()
-
-    user_id =  json_response.get("data").get("user").get("result").get("rest_id")
-
-    return user_id
 
 
 
